c0029ec4a882384098a7578f71c31051e313dba9
Nicolás Ballén <n.ballen1@uniandes.edu.co>
2024-12-01 17:21:33 -0500

    detalles

--- src/main/java/uniandes/edu/co/proyecto/controller/ProductoController.java
+++ src/main/java/uniandes/edu/co/proyecto/controller/ProductoController.java
@@ -23,7 +23,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import uniandes.edu.co.proyecto.modelo.Almacenaje;
 import uniandes.edu.co.proyecto.modelo.Producto;
 import uniandes.edu.co.proyecto.modelo.Secuencia;
 import uniandes.edu.co.proyecto.repository.CategoriaRepository;
